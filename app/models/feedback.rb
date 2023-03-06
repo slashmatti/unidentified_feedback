c47c6a336597c11aa2581d0eb599d026ce6befdd
Matti Salimi <mattisalimi@gmail.com>
2023-03-06 20:40:04 +0100

    client- and server-side validations

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,13 @@
 class Feedback < ApplicationRecord
+    before_create :sanitize
     after_create :send_tweet
+
+    validates_presence_of :recipient_handle, text
+    validates_length_of :text, minimum: 1, maximum: 250
+
+    def sanitize
+        self.recipient_handle = self.recipient_handle.gsub('@', '')
+    end
 
     def send_tweet
         puts "Sending tweet! @#{self.recipient_handle}, '#{self.text}'"

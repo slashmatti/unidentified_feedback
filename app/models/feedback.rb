c1504816693fece59a2d43e7fe9bc65c1d763acb
Matti Salimi <mattisalimi@gmail.com>
2023-03-07 13:26:46 +0100

    fixed bug

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,7 +2,7 @@ class Feedback < ApplicationRecord
     before_create :sanitize
     after_create :send_tweet
 
-    validates_presence_of :recipient_handle, text
+    validates_presence_of :recipient_handle, :text
     validates_length_of :text, minimum: 1, maximum: 250
 
     def sanitize
